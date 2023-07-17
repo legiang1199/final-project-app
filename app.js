677f9ffec3f801c3c7bb1bad93aff4ed82cdd269
legiang1199 <legiang1199@gmail.com>
2023-07-17 20:52:15 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const { connectDatabase } = require('./database/connect')
+
+var indexRouter = require('./routes/index')
+
+
+var app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+
+
+// /users/
+
+//connect to database
+;(async () => {
+    await connectDatabase()
+})()
+
+module.exports = app
